184dcd4a9fd119a8daff076f3768d6d413995b70
Gilles Jacobs <gillesjac@gmail.com>
2014-11-03 14:51:40 +0100

    model

---
+++ db/migrate/20141103135120_create_quotes.rb
@@ -0,0 +1,16 @@
+class CreateQuotes < ActiveRecord::Migration
+  def change
+    create_table :quotes do |t|
+      t.string :token
+      t.string :team_id
+      t.string :channel_id
+      t.string :channel_name
+      t.string :user_id
+      t.string :user_name
+      t.string :command
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
