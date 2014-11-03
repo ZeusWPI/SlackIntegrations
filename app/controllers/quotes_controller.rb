02d0b1e353fcb75bb07830eb014a77d1e29bd089
Gilles Jacobs <gillesjac@gmail.com>
2014-11-03 17:31:57 +0100

    tis af joenge

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,6 @@
 class QuotesController < ApplicationController
+  respond_to :html
+
   def index
     @quotes = Quote.all
 
@@ -7,6 +9,47 @@ class QuotesController < ApplicationController
 
   def create
     puts params
+    @quote = Quote.new quote_params
+
+    @quote.save
     redirect_to root_url
   end
+
+  def show
+    text = params[:text]
+    if numeric?(params[:text])
+      id = params[:text].to_i
+
+      @quote = Quote.find_by(id: id)
+
+      render plain: @quote.text if !@quote.nil?
+      return
+    end
+
+    @quotes = Quote.where('text like ?', "%#{params[:text]}%")
+    if !@quotes.empty?
+      render plain: @quotes.shuffle.first.text
+    else
+      render plain: 'Oei der zijn der zo geen!'
+    end
+  end
+
+  private
+    def quote_params
+      params.require(:quote).permit(:token,
+                                    :team_id,
+                                    :channel_id,
+                                    :channel_name,
+                                    :user_id,
+                                    :user_name,
+                                    :command,
+                                    :text)
+    end
+
+    # Whats a rails project without a stuckoverflow copy paste?
+    # http://stackoverflow.com/questions/5661466/test-if-string-is-a-number-in-ruby-on-rails
+    def numeric?(str)
+      return true if str =~ /^\d+$/
+      true if Float(str) rescue false
+    end
 end
