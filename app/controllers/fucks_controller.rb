6d2accc37b0d54a0675926219206e09cc5bfceae
benji <benj_cous@hotmail.com>
2015-02-17 12:12:49 +0100

    remove debug info

--- app/controllers/fucks_controller.rb
+++ app/controllers/fucks_controller.rb
@@ -13,11 +13,6 @@ class FucksController < ApplicationController
   def show
     name = Fuck.format(fuck_params[:text])
 
-    if true
-      render json: { text: name }
-      return
-    end
-
     fuck = Fuck.find_by_name(name.downcase)
     if fuck.nil?
       render json: { }
