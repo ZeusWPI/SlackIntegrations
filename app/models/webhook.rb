6913a5b2c76e442adb7932295a92da3d46c6eaba
benji <benj_cous@hotmail.com>
2015-10-27 20:42:22 +0100

    typo

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -14,7 +14,7 @@ class Webhook
 
     self.hook = Tarumi::Bot.new(
       SlackIntegrations::ZEUS_TEAM,
-      Rails.application.secret.zeus_token,
+      Rails.application.secrets.zeus_token,
       options
     )
   end
